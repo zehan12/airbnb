df9bd2ff6aa4656b774c8a4e8dcd5e27d8124338
Zehan Khan <zkhan@hootboard.com>
2024-03-30 05:59:33 +0530

    feat: 🌊 integrate tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,14 @@ import { Fragment } from "react/jsx-runtime";
 const App = () => {
   return (
     <Fragment>
-      <h1>Airbnb</h1>
+      <main className="container mx-auto mt-28 px-5 flex flex-col">
+        <h1 className="text-4xl">Airbnb</h1>
+        <p className="w-80 p-4">
+          AIRBNB IS AN ONLINE MARKETPLACE THAT CONNECTS PEOPLE WHO WANT | TO
+          RENT OUT THEIR PROPERTY WITH PEOPLE WHO ARE LOOKING FOR |
+          ACCOMMODATIONS, TYPICALLY FOR SHORT STAYS
+        </p>
+      </main>
     </Fragment>
   );
 };
